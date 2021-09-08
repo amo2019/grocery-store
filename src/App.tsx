20d2d285fdec1110598bc21837bd126264da3961
Amin Omar <aminvpython@gmail.com>
2021-09-08 13:21:31 +0200

    add useEffect, useState to seed the app with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+import type { Product } from "./product";
+import { HomePage } from "./components/HomePage";
+import gproducts from "./products.json"
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  let gsproducts: Product[] = gproducts;
+  useEffect(() => {
+    setProducts(gsproducts)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <HomePage
+            products={products}
+          />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
