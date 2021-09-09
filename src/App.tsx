90315908804de8c7a0fa692ce95201d9755bf138
Amin Omar <aminvpython@gmail.com>
2021-09-09 16:26:00 +0200

    add cart feature (on add & on clear cart)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { useEffect, useState, useCallback } from "react";
+import { useLocalStorage } from "react-use";
 import { Switch, Route, useHistory, useLocation } from "react-router-dom";
-import { Header } from "./components/Header";
+
 import "./App.css";
 
 import type { Product } from "./product";
 import { HomePage } from "./components/HomePage";
 import { ProductDetail } from "./components/ProductDetail";
+import { Header } from "./components/Header";
 import gproducts from "./products.json"
 
 function App() {
+  const [cart, setCart] = useLocalStorage<Product[]>("cart", []);
   const [products, setProducts] = useState<Product[]>([]);
   let gsproducts: Product[] = gproducts;
+  console.log("gproducts: ", gproducts)
+  const onAddToCart = useCallback(
+    (product: Product) => {
+      setCart([...(cart ?? []), product]);
+    },
+    [cart, setCart]
+  );
+  const onClearCart = useCallback(() => {
+    setCart([]);
+  }, [setCart]);
+  //const setDefaultPruducts = (gsproducts)=>setProducts(gsproducts)
   useEffect(() => {
     setProducts(gsproducts)
   }, []);
@@ -31,6 +45,8 @@ function App() {
   return (
     <div>
       <Header
+      cart={cart ?? []}
+      onClearCart={onClearCart}
       search={search}
       onSetSearch={onSetSearch}
       />
@@ -38,11 +54,12 @@ function App() {
         <Route exact path="/">
           <HomePage
             products={products}
+            onAddToCart={onAddToCart}
             search={search}
           />
         </Route>
         <Route path="/detail/:id">
-          <ProductDetail products={products}  />
+          <ProductDetail products={products} onAddToCart={onAddToCart}  />
         </Route>
 
       </Switch>
