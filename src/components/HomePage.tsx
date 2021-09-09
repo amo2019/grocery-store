bb367ecc66b7af51ce2789d5401b7fec6b911f6f
Amin Omar <aminvpython@gmail.com>
2021-09-09 16:32:14 +0200

    modify jsx part

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,8 +5,9 @@ import "./HomePage.css";
 
 export const HomePage: React.FunctionComponent<{
   products: Product[];
+  onAddToCart: (product: Product) => void;
   search: string;
-}> = ({ products, search }) => {
+}> = ({ products, onAddToCart, search }) => {
   const filteredProducts = useMemo(
     () =>
       (products ?? []).filter(
@@ -26,29 +27,39 @@ export const HomePage: React.FunctionComponent<{
         <Link
           to={`/detail/${product.id}`}
           key={product.id}
+          className="flex-cart m-2 border border-1 border-green-600 rounded-t-lg bg-green-600"
         >
-          <h3 >
+          <h3 className="title font-bold truncate w-full max-w-full py-2 px-4 text-white">
             {product.title}
           </h3>
-          <div >
-            <div >
+          <div className="bg-white p-3">
+            <div className="w-full flex justify-center">
               <img src={product.image} alt={product.title}/>
             </div>
-            <div>
-              <div >
+            <div className="bg-white p-3">
+              <div className="w-full flex justify-center">
                 {product.description}
               </div>
               <div className="flex-container">
-                <div >{product.category}</div>
-                <div className="flex-grow justify-end">
+                <div className="text-md mt-1">{product.category}</div>
+                <div className="text-lg text-right flex-grow justify-end mt-1 mr-4 font-bold">
                   {product.price.toLocaleString("en-US", {
                     style: "currency",
                     currency: "USD",
                   })}
                 </div>
-                
+                <button
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    onAddToCart(product);
+                  }}
+                  className="text-lg px-8 py-1 font-bold bg-green-800 text-white rounded-full"
+                >
+                  <i className="fas fa-cart-plus mr-2"></i>
+                  Add To Cart
+                </button>
               </div>
-            </div>
+              </div>
           </div>
         </Link>
       ))}
