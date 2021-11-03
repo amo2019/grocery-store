0729ed831a366d117753c43d63b98fe7156a4e8a
Amin Omar <aminvpython@gmail.com>
2021-11-03 22:40:41 +0100

    add uuid &  productsSelector to manage the state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import type { Product } from "../product";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
 import "./Header.css";
 
 export const Header: React.FunctionComponent<{
@@ -13,10 +15,19 @@ export const Header: React.FunctionComponent<{
     () => (cart ? cart.reduce((total, p) => total + p.price, 0) : 0),
     [cart]
     );
-
+  
+   const productsSelector = useSelector(
+    (state: RootState) => state.products.item
+  );
+  let uuid = () => {
+    return (`[1e7]+-1e3+-4e3+-8e3+-1e11`).replace(/[018]/g, (c: any) =>
+      (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & 15) >> c / 4).toString(16)
+    );
+  }
+  console.log("productsSelector:", productsSelector)
 return (
     <div className="header">
-    <div className="text-2xl px-10 py-2">
+    <div onClick={()=>onSetSearch("")} className="text-2xl px-10 py-2">
         <Link to="/" data-testid="header">
         <i className="fas fa-shopping-basket mr-2"></i>
         Simple Grocery Store
@@ -31,12 +42,11 @@ return (
         className="input p-2 text-xl  rounded-lg max-w-md w-96"
         />
     </div>
-
     <div className="px-10 py-2 justify-end has-tooltip cart-icon ">
         <span className="tooltip cart">
         <div>Cart ({(cart ?? []).length})</div>
         {cart?.map((product, index) => (
-        <div key={`${product.id}${index}`} className="flex flex-row my-2">
+        <div key={uuid()} className="flex flex-row my-2">
             <img src={product.image} alt={product.title} className="h-8 mr-2" />
             <h3 className="title text-md truncate flex-grow">{product.title}</h3>
             <div className="text-md text-right flex-grow justify-end ml-2">
